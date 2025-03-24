adf108a4b1ae89f353682d0b2904cf1628459601
TomyMarengo <marengotomas@gmail.com>
2025-03-24 19:51:05 -0300

    Fix some issues

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,10 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from '@/App';
-import { store } from '@/services/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
